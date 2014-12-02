994fb7a899e294f2c38d0bebf04fdba934cb2f4d
Wolfgang Puchinger <tpuchinger@web.de>
2014-12-02 16:14:26 +0100

    refactored modul

--- run.py
+++ run.py
@@ -4,27 +4,28 @@ from maxcube import parsing
 from maxcube import output
 from maxcube import network
 from maxcube import objects
-
-from udp_handshake import udp_handshake
+from maxcube import discover
 
 
 def main():	
-	tcp_addr, tcp_port = udp_handshake()
-	raw_data = network.read_raw_data(tcp_addr, tcp_port)
+	print("searching maxcube(s)..")
 
-	print('')
-	print('raw_data:')
-	output.display(raw_data)
-	
-	print('')
-	print('parsed_data:')
-	parsed_data = parsing.start(raw_data)
-	output.display(parsed_data)
-	
-	print('')
-	print('Cube:')
-	cube = objects.from_parsed_data(parsed_data)
-	output.display(cube)
+	for tcp_addr, tcp_port in discover.scan()
+		raw_data = network.read_raw_data(tcp_addr, tcp_port)
+
+		print('')
+		print('raw_data:')
+		output.display(raw_data)
+		
+		print('')
+		print('parsed_data:')
+		parsed_data = parsing.start(raw_data)
+		output.display(parsed_data)
+		
+		print('')
+		print('Cube:')
+		cube = objects.from_parsed_data(parsed_data)
+		output.display(cube)
 	
 if __name__ == '__main__':
 	main()
