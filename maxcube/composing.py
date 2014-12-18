6ac8ec8019fb05be3d05368d1b5eb6340fae94f3
Wolfgang Puchinger <tpuchinger@web.de>
2014-12-18 23:56:11 +0100

    removed debug code

--- maxcube/composing.py
+++ maxcube/composing.py
@@ -45,24 +45,3 @@ def compile_s(rf_adress, room_id, temp, mode, date_until, time_until):
 	message += time_encoded
 
 	return message
-
-
-
-print('-- Test 1 --')
-print(b's:AARAAAAAAP4wAaiLix8=\r\n')
-print(compose_s(bytes([0x00, 0xfe, 0x30]), 1, 20, 2, datetime.date(2011, 9, 11), datetime.time(15, 30)))
-
-
-print('-- Test 2 --')
-print([hex(i) for i in [0x00, 0x04, 0x40, 0x00, 0x00, 0x00, 0x00, 0xFE, 0x30, 0x01, 0x00]])
-print([hex(i) for i in compile_s([0x00, 0xfe, 0x30], 1, 0, 0, None, None)])
-
-print(b's:AARAAAAAAP4wAQA=\r\n')
-print(compose_s(bytes([0x00, 0xfe, 0x30]), 1, 0, 0, None, None))
-
-
-print('-- Test 3 --')
-print([hex(i) for i in [0x00, 0x04, 0x40, 0x00, 0x00, 0x00, 0x00, 0xFE, 0x30, 0x01, 0x6C]])
-print([hex(i) for i in compile_s(bytes([0x00, 0xfe, 0x30]), 1, 22, 2, None, None)])
-print([hex(i) for i in compile_s(bytes([0x00, 0xfe, 0x30]), 1, 22, 1, None, None)])
-print([hex(i) for i in compile_s(bytes([0x00, 0xfe, 0x30]), 1, 22, 0, None, None)])
