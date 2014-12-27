e53ea41451e4ef92feb1c75b9cd68d875c14b90d
Wolfgang Puchinger <tpuchinger@web.de>
2014-12-27 02:22:31 +0100

    reworked output for better visibility in terminals

--- maxcube/output.py
+++ maxcube/output.py
@@ -1,9 +1,26 @@
 # -*- coding: utf-8 -*-
 
 from pprint import pprint
+from maxcube.constants import Constant
+
+def fformat(obj):
+	if isinstance(obj, list):
+		ret = []
+		for i in obj:
+			ret.append(fformat(i))
+	elif isinstance(obj, dict):
+		ret = {}
+		for key, value in obj.items():
+			ret[key] = fformat(value)
+	elif isinstance(obj, Constant):
+		ret = obj
+	else:
+		try:
+			ret = fformat(obj.__dict__)
+		except:
+			ret = obj
+
+	return ret
 
 def display(s):
-	try:
-		display(s.__dict__)
-	except:
-		pprint(s)
+	pprint(fformat(s))
